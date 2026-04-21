34a610f643a2b30cd7377ee0abab70d406359ebf
pengstao-creator <pengstao-creator.com>
2026-04-21 17:12:52 +0800

    中断相关

--- User/main.c
+++ User/main.c
@@ -1,20 +1,8 @@
 #include "stm32f10x.h"
-#include "buzz.h"
-// #include "bindKey.h"
 #include "exti.h"
 int main()
 {
-	buzzInit();
-  // bindKeyInit();
-  extiInit();
   while (1)
   {
-    // u8 num = monitorMbutton();
-    // if(num != NOT_P)
-    // {
-    //   bindKey(num);
-    //   // vibFreqCtrl(250,100);
-    // }
-    show();
   }
 }
